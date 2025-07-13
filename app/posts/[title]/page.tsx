fa2464c5567e2b06665016f858805e520c6f46d7
miguelagsilva <80349903+miguelagsilva@users.noreply.github.com>
2025-07-13 01:25:10 +0100

    feat: Create initial pages

---
+++ app/posts/[title]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto p-4">
+        <h1>This is a post</h1>
+        </div>
+  );
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto p-4">
+      <p>Here are all my posts.</p>
+    </div>
+  );
+}
